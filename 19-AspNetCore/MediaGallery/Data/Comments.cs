350a18173153f5e71c21bef7c9b55b7477433a21
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-11-14 14:05:31 +0200

    initial commit

---
+++ 19-AspNetCore/MediaGallery/Data/Comments.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaGallery.Data
+{
+    public class Comments
+    {
+        public IdentityUser UserId { get; set; }
+        public MediaItem MediaItemId { get; set; }
+    }
+}
